c7904e39e82dfc804e66f96bbf7029fd601570e3
Qian Gong <gongqian.de@gmail.com>
2026-03-14 02:28:08 +0100

    implement pig latin logic

---
+++ pig-latin.js
@@ -0,0 +1,22 @@
+// https://en.wikipedia.org/wiki/Pig_Latin
+
+const args = process.argv[2]?.trim().toLowerCase();
+
+const suffix = "ay";
+
+let str = "";
+
+const quMatch = args.match(/^s?qu/);
+if (quMatch) {
+  const prefix = quMatch[0];
+  str = args.substring(prefix.length) + "-" + prefix + suffix;
+} else {
+  const prefix = args.split(/[aeiou]+/, 1)[0];
+  if (!prefix) {
+    str = args + "-w" + suffix;
+  } else {
+    str = args.replace(prefix, "") + "-w" + prefix + suffix;
+  }
+}
+
+console.log(str);
