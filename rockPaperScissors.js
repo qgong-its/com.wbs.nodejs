421aa3c30fdaefef304e61bd1abe1aab2e0f28e9
Qian Gong <gongqian.de@gmail.com>
2026-03-14 23:51:40 +0100

    Modify console output

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,15 +4,18 @@ const userIdx = choices.indexOf(process.argv[2]?.toLowerCase());
 
 const cpuIdx = Math.floor(Math.random() * 3);
 
-if (userIdx === -1) return console.log("ERROR: Invalid input");
+if (userIdx === -1)
+  return console.log("ERROR: Invalid input. Use: rock | paper | scissors");
+
+console.log(`You chose: ${choices[userIdx]}`);
 
 console.log(`CPU chose: ${choices[cpuIdx]}`);
 
 if (userIdx === cpuIdx) {
-  console.log("Draw");
+  console.log("Result: Draw");
 } else if ((userIdx - cpuIdx + 3) % 3 === 1) {
   // 索引差值为 1 或 -2 时玩家赢
-  console.log("Win");
+  console.log("Result: Win");
 } else {
-  console.log("Lose");
+  console.log("Result: Lose");
 }
